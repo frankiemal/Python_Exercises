62dfe8251f24bfb926f729db734cd0d40b3aad3d
frankiemal <xi20868@bristol.ac.uk>
2023-01-27 11:53:36 +0000

    Create Exercise1a.py
    Exercise 1A - W13

---
+++ Exercise1a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 11:24:20 2023
+
+@author: frankiemalinowski
+"""
+
+#Pi = 0
+#def Leibniz(n):
+#    Pi = 0 
+#    for N in range(n):
+#        Pi += 8 / ((4*N + 1) * (4*N + 3))
+#        if N == n:
+ #           return print(Pi)
+            
+#Leibniz(100)
+    
+Pi = 0
+for N in range(100):
+    Pi += 8 / ((4*N + 1) * (4*N + 3))
+    print(Pi)
