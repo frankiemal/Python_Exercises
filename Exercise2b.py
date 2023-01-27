c824387a05403e43a58016d051209164d534c046
frankiemal <xi20868@bristol.ac.uk>
2023-01-27 12:34:05 +0000

    Create Exercise2b.py
    Q2B W13

---
+++ Exercise2b.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 12:30:03 2023
+
+@author: frankiemalinowski
+"""
+
+import numpy as np
+
+def Matrix_Multiplication(a,b):
+    product = np.matmul(a,b)
+    return print(product)
+
+a = np.array([[1,2],[3,4]])
+b = np.array([[1,2],[3,4]])
+
+Matrix_Multiplication(a,b)
+
+#print(np.dot(a[0],a[1]))
